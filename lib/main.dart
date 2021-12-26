4b17f077ee8098868907b41c8e7fd632f41f682e
Kaze Shindo <kaze.bknb@outlook.jp>
2021-12-26 16:01:32 +0900

    add: fimber

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,23 @@
+import 'dart:async';
+
+import 'package:fimber/fimber.dart';
 import 'package:flapp/app.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(const ProviderScope(child: MyApp()));
+  // Fimber
+  if (!kReleaseMode) {
+    Fimber.plantTree(DebugTree());
+  } else {
+    debugPrint = (message, {wrapWidth}) {};
+  }
+
+  runZonedGuarded(
+    () => runApp(const ProviderScope(child: MyApp())),
+    (error, stackTrace) {
+      Fimber.e(error.toString());
+    },
+  );
 }
